5c3fc59bdc70b16f994c35ac8a7bf768eb4b3218
Henry Le <henryle@Hieus-MacBook-Pro.local>
2023-03-01 02:37:07 +0700

    Adding my first ReactJS project

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,9 +5,9 @@ export default function main(){
         <main>
             <h1>Fun facts about react</h1>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan Walker</li>
+                <li>Has well over</li>
                 <li></li>
                 <li></li>
             </ul>
