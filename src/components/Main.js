fe7c98c76fad922a483e80bc44c2be69aea11f1b
Henry Le <henryle@Hieus-MacBook-Pro.local>
2023-03-01 02:35:29 +0700

    Adding my first project

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function main(){
+    return (
+        <main>
+            <h1>Fun facts about react</h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </main>
+    )
+}
