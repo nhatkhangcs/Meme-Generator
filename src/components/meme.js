a8f1642298b57a04b92fe05fbb6784ecb81bf4ad
Henry Le <henryle@Hieus-MacBook-Pro.local>
2023-03-06 15:51:48 +0700

    finish meme generator

--- src/components/meme.js
+++ src/components/meme.js
@@ -18,17 +18,39 @@ export default function Meme() {
                 randomImage: memesImg[i].url
             }
         })
-
+    }
+    function handleChange(event) {
+        setMeme(prevMeme => {
+            const {name, value} = event.target
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
     }
     return (
         <main>
             <form className="form--block">
-                <input type="text" className="form--input" placeholder="Top text" />
-                <input type="text" className="form--input" placeholder="Bottom text" />
+                <input
+                    type="text"
+                    className="form--input"
+                    placeholder="Top text"
+                    name="topText"
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    className="form--input"
+                    placeholder="Bottom text"
+                    name="bottomText"
+                    onChange={handleChange}
+                />
                 <button type="button" onClick={randomFunction} className="form--button">Get a new meme image  🖼</button>
             </form>
             <div className="form--meme">
                 <img src={meme.randomImage} width="100%" className="form--meme-img"></img>
+                <h2 className="form--meme-text top">{meme.topText}</h2>
+                <h2 className="form--meme-text bottom">{meme.bottomText}</h2>
             </div>
         </main>
     )
